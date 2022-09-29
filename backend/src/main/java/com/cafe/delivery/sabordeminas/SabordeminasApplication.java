7d91b7893b0f83df91c0a55d884a9076f8e0a1cd
Jhonatamsoares <git@github.com:Jhonatamsoares/dsmeta.git>
2022-09-28 21:57:57 -0300

    projeto criado com conexão postgress

---
+++ backend/src/main/java/com/cafe/delivery/sabordeminas/SabordeminasApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.delivery.sabordeminas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabordeminasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabordeminasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cafe/delivery/sabordeminas/SabordeminasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.delivery.sabordeminas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabordeminasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
